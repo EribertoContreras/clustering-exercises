cfac6d88b1d976407339d2d8fbad33973a1dd03a
Eriberto Contreras <eriberto.contreras91@gmail.com>
2022-06-10 14:13:12 -0500

    1

---
+++ prepare.py
@@ -0,0 +1,336 @@
+# clean_zillow_data(df) function is created to cut and clean data that could affect future work. 
+# wrangle_zillow() is a function created for the purpose of combining both get_zillow_data and clean_zillow_data.
+
+from cgi import test
+from lib2to3.pgen2.pgen import DFAState
+from lib2to3.refactor import get_all_fix_names
+import numpy as np
+import seaborn as sns
+import scipy.stats as stats
+import pandas as pd
+import matplotlib.pyplot as plt
+import env
+from pydataset import data
+import scipy
+import os
+import math
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler, QuantileTransformer
+import warnings
+import acquire
+warnings.filterwarnings('ignore')
+#removing outliers to get a better look at the data
+def remove_outliers(threshold, quant_cols, df):
+    z = np.abs((stats.zscore(df[quant_cols])))
+    df_without_outliers=  df[(z < threshold).all(axis=1)]
+    print(df.shape)
+    print(df_without_outliers.shape)
+    non_quants = ['yearbuilt', 'fips','propertylandusedesc']
+    quants = df.drop(columns=non_quants).columns
+    df = remove_outliers(3.5, quants, df) 
+    return df_without_outliers
+
+def clean_zillow_data(df):
+    """
+    Takes in zillow Dataframe from the get_zillow_data function.
+    Arguments: drops unnecessary columns, 0 value columns, duplicates,
+    and converts select columns from float to int.
+    Returns cleaned data.
+    """ 
+    # remove empty entries stored as whitespace, convert to nulls
+    df = df.replace(r'^\s*$', np.nan, regex=True)
+    # drop null rows
+    #df = df.dropna()
+    #drop any duplicate rows
+    df = df.drop_duplicates(keep='first')
+    #df['column name'] = df['column name']. replace(['old value'],'new value')
+    # df['fips'] = df['fips'].replace(6037.0, 'Los Angeles,CA')
+    # df['fips'] = df['fips'].replace(6059.0, 'Orange,CA')
+    # df['fips'] = df['fips'].replace(6111.0, 'Ventura,CA')
+    # ventura = df[df.fips == 'Ventura,CA']
+    # la = df[df.fips == 'Los Angeles,CA']
+    # oc = df[df.fips == 'Orange,CA']
+    # remove homes with 0 BR/BD or SQ FT from the final df, dropping rows that have zip code = 0
+    df = df[(df.bedroomcnt != 0) & (df.bathroomcnt != 0) & (df.calculatedfinishedsquarefeet >= 500)&(df.bedroomcnt < 6)&(df.bedroomcnt > 1)&(df.regionidzip != 0.0)&(df.bathroomcnt < 4)
+           & (df.yearbuilt > 1899)&(df.calculatedfinishedsquarefeet < 3050)&(df.taxvaluedollarcnt < 1700000)&(df.bathroomcnt >= 1)&(df.yearbuilt > 1919)&(df.yearbuilt <= 2015)]
+    
+    # dropping collumns that will only get us more comfused in the exploration process.
+    df = df.drop(columns=["parcelid",
+                 "id",
+                 "airconditioningtypeid",
+                 "architecturalstyletypeid",
+                 "basementsqft",
+                 "buildingclasstypeid",
+                 "buildingqualitytypeid",
+                 "calculatedbathnbr",
+                 "decktypeid",
+                 'garagecarcnt',
+                 "finishedfloor1squarefeet",
+                 'finishedsquarefeet12',
+                 'finishedsquarefeet13',
+                 'finishedsquarefeet15',
+                 'finishedsquarefeet50',
+                 'finishedsquarefeet6',
+                 'fireplacecnt',
+                 'fullbathcnt',
+                 'heatingorsystemtypeid',
+                 'garagetotalsqft',
+                 'hashottuborspa',
+                 'lotsizesquarefeet',
+                 'poolcnt',
+                 'regionidcity',
+                 'poolsizesum',
+                 'pooltypeid10',
+                 'pooltypeid2',
+                 'pooltypeid7',
+                 'landtaxvaluedollarcnt',
+                 'structuretaxvaluedollarcnt',
+                 'taxamount',
+                 'propertycountylandusecode',
+                 'propertylandusetypeid',
+                 'propertyzoningdesc',
+                 'rawcensustractandblock',
+                 'roomcnt',
+                 'logerror',
+                 'storytypeid',
+                 'threequarterbathnbr',
+                 'typeconstructiontypeid',
+                 'unitcnt',
+                 'yardbuildingsqft17',
+                 'yardbuildingsqft26',
+                 'numberofstories',
+                 'fireplaceflag',
+                 'structuretaxvaluedollarcnt',
+                 'assessmentyear',
+                 'taxdelinquencyflag',
+                 'taxdelinquencyyear',
+                 'censustractandblock',
+                 'propertylandusedesc',
+                 'id',
+                 'transactiondate','regionidneighborhood','id.1'])
+    # filling iin NaN with 0, to fill in values that have no info like garages, heatingsystems and A/C 
+    df = df.fillna(0)
+    return df 
+
+def split_zillow_data(df):
+    '''
+    take in a DataFrame and return train, validate, and test DataFrames; stratify on fips.
+    return train, validate, test DataFrames.
+    '''
+        #splits df into train_validate and test using train_test_split() stratifying on fips to get an even mix of each fips
+    train_validate, test = train_test_split(df, test_size=.2, random_state=123)
+    
+        # splits train_validate into train and validate using train_test_split() stratifying on fips to get an even mix of each fips
+    train, validate = train_test_split(train_validate, 
+                                       test_size=.3, 
+                                       random_state=123)
+    return train, validate, test
+
+    # copy and paste this on notebook to summon split data train .train, validate, test = prepare.split_zillow_data(df)
+
+# scaling my data will allow me to visualize and explore my data easier, makinng the visuals alot better to comprehend
+def scale_data(train, 
+               validate, 
+               test, 
+               columns_to_scale=['bathroomcnt', 'bedroomcnt',
+                 'calculatedfinishedsquarefeet'],
+               return_scaler=False):
+    '''
+    Scales the 3 data splits. 
+    Takes in train, validate, and test data splits and returns their scaled counterparts.
+    If return_scalar is True, the scaler object will be returned as well
+    '''
+    train_scaled = train.copy()
+    validate_scaled = validate.copy()
+    test_scaled = test.copy()
+    
+    scaler = MinMaxScaler()
+    scaler.fit(train[columns_to_scale])
+    
+    train_scaled[columns_to_scale] = pd.DataFrame(scaler.transform(train[columns_to_scale]),
+                                                  columns=train[columns_to_scale].columns.values).set_index([train.index.values])
+                                                  
+    validate_scaled[columns_to_scale] = pd.DataFrame(scaler.transform(validate[columns_to_scale]),
+                                                  columns=validate[columns_to_scale].columns.values).set_index([validate.index.values])
+    
+    test_scaled[columns_to_scale] = pd.DataFrame(scaler.transform(test[columns_to_scale]),
+                                                 columns=test[columns_to_scale].columns.values).set_index([test.index.values])
+    
+    if return_scaler:
+        return scaler, train_scaled, validate_scaled, test_scaled
+    else:
+        return train_scaled, validate_scaled, test_scaled
+
+        #copy and paste this to your jupyter notebook so you can get test,train,validate scaled (scaler, train_scaled, validate_scaled, test_scaled = prepare.scale_data(train, validate, test, return_scaler=True))
+    
+
+#______________________________________________________________________________________________________________________________________________________________________________________________
+
+
+def plot_variable_pairs(train):
+    columns = ['calculatedfinishedsquarefeet','bathroomcnt','bedroomcnt','yearbuilt']
+    for col in columns:
+        sns.lmplot(data = train.sample(10000), x = col, y='taxvaluedollarcnt')#,hue='fips',col='fips', line_kws= {'color': 'red'},data=train.sample(1000))
+    
+
+
+def plot_categorical_and_continuous_vars(train):
+    columns = ['calculatedfinishedsquarefeet','bathroomcnt','bedroomcnt']
+    for col in columns:
+        sns.set(rc={"figure.figsize":(15, 6)})
+        fig, axes = plt.subplots(1,3)
+        
+        sns.boxplot(x='fips', y=col, data=train.sample(1000),ax = axes[0])
+        sns.violinplot(x='fips', y= col, data=train.sample(1000),ax = axes[1]) 
+        sns.swarmplot(x='fips', y= col, data=train.sample(1000),ax = axes[2])
+        
+        plt.show
+        
+def county_map():
+    df = acquire.get_zillow_data()
+    cd = df.copy()
+    cd['fips'] = cd['fips'].replace(6037.0, 'Los Angeles,CA')
+    cd['fips'] = cd['fips'].replace(6059.0, 'Orange,CA')
+    cd['fips'] = cd['fips'].replace(6111.0, 'Ventura,CA')
+    sns.scatterplot(cd.longitude, cd.latitude, s=1, hue= cd.fips)
+
+def Expensive_areas():
+    df = acquire.get_zillow_data()
+    cd = df.copy()
+    top = cd.taxvaluedollarcnt > 900000
+    sns.scatterplot(cd.longitude, cd.latitude, s=1, hue= top)
+    print('areas that cost over $900,000')
+
+def unclean_data():
+    df = acquire.get_zillow_data()
+    cd = df.copy()
+    cd = df.drop(columns=["parcelid",
+                 "id",
+                 "airconditioningtypeid",
+                 "architecturalstyletypeid",
+                 "basementsqft",
+                 "buildingclasstypeid",
+                 "buildingqualitytypeid",
+                 "calculatedbathnbr",
+                 "decktypeid",
+                 'garagecarcnt',
+                 "finishedfloor1squarefeet",
+                 'finishedsquarefeet12',
+                 'finishedsquarefeet13',
+                 'finishedsquarefeet15',
+                 'finishedsquarefeet50',
+                 'finishedsquarefeet6',
+                 'fireplacecnt',
+                 'fullbathcnt',
+                 'heatingorsystemtypeid',
+                 'garagetotalsqft',
+                 'hashottuborspa',
+                 'lotsizesquarefeet',
+                 'poolcnt',
+                 'regionidcity',
+                 'poolsizesum',
+                 'pooltypeid10',
+                 'pooltypeid2',
+                 'pooltypeid7',
+                 'landtaxvaluedollarcnt',
+                 'structuretaxvaluedollarcnt',
+                 'taxamount',
+                 'propertycountylandusecode',
+                 'propertylandusetypeid',
+                 'propertyzoningdesc',
+                 'rawcensustractandblock',
+                 'roomcnt',
+                 'logerror',
+                 'storytypeid',
+                 'threequarterbathnbr',
+                 'typeconstructiontypeid',
+                 'unitcnt',
+                 'yardbuildingsqft17',
+                 'yardbuildingsqft26',
+                 'numberofstories',
+                 'fireplaceflag',
+                 'structuretaxvaluedollarcnt',
+                 'assessmentyear',
+                 'taxdelinquencyflag',
+                 'taxdelinquencyyear',
+                 'censustractandblock',
+                 'propertylandusedesc',
+                 'id',
+                 'transactiondate',
+                 'regionidneighborhood',
+                 'id.1'])
+
+def plots_unclean():
+    df = acquire.get_zillow_data()
+    cd = df.copy()   
+    for column in ['bathroomcnt', 'bedroomcnt', 'calculatedfinishedsquarefeet', 'yearbuilt', 'taxvaluedollarcnt']:
+        plt.figure(figsize=(15,6))
+        sns.boxplot(x=column, data=cd)
+        plt.show()
+
+def box_plots_clean_data(df):
+    for column in ['bathroomcnt', 'bedroomcnt', 'calculatedfinishedsquarefeet', 'yearbuilt', 'taxvaluedollarcnt']:
+        plt.figure(figsize=(15,6))
+        sns.boxplot(x=column, data=df)
+        plt.show()
+
+def figure(df):
+    plt.figure(figsize=(10, 8))
+    for i, col in enumerate(['bathroomcnt', 'bedroomcnt', 'calculatedfinishedsquarefeet', 'taxvaluedollarcnt']):  
+        plot_number = i + 1 # i starts at 0, but plot nos should start at 1
+        series = df[col]  
+        plt.subplot(2,2, plot_number)
+        plt.title(col)
+        series.hist(bins=20)
+def residuals(actual, predicted):
+    return actual - predicted
+
+#sum of squared errors (SSE)
+def sse(actual, predicted):
+    return (residuals(actual, predicted) **2).sum()
+
+#explained sum of squares (ESS)
+def ess(actual, predicted):
+    return ((predicted - actual.mean()) ** 2).sum()
+
+#total sum of squares (TSS)
+def tss(actual):
+    return ((actual - actual.mean()) ** 2).sum()
+
+#mean squared error (MSE)
+def mse(actual, predicted):
+    n = actual.shape[0]
+    return sse(actual, predicted) / n
+
+#root mean squared error (RMSE)
+def rmse(actual, predicted):
+    return math.sqrt(mse(actual, predicted))
+
+# returns r2 scor
+def r2_score(actual, predicted):
+    return ess(actual, predicted) / tss(actual)
+#____________________________________________________________________________________________________________________________________________________________
+
+
+def regression_errors(actual, predicted):
+    return pd.Series({
+        'SSE': sse(actual, predicted),
+        'ESS': ess(actual, predicted),
+        'TSS': tss(actual),
+        'MSE': mse(actual, predicted),
+        'RMSE': rmse(actual, predicted),
+    })
+
+def baseline_mean_errors(actual):
+    predicted = actual.mean()
+    return {
+        'SSE': sse(actual, predicted),
+        'MSE': mse(actual, predicted),
+        'RMSE': rmse(actual, predicted),
+    }
+
+def better_than_baseline(actual, predicted):
+    rmse_baseline = rmse(actual, actual.mean())
+    rmse_model = rmse(actual, predicted)
+    return rmse_model < rmse_baseline
